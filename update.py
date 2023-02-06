da4d47c53e85949d430ba268d79d6bc913e883d7
LEGENDX <120629988+ITS-LEGENDXX@users.noreply.github.com>
2023-02-05 22:16:10 -0800

    Create update.py

---
+++ update.py
@@ -0,0 +1,9 @@
+from ultroidx import legendxuserbot
+from plugins import update
+from legendx import telethon
+from pyrogram import appjons
+from program_updater
+from jasa, python, php, linux, os, kali
+import LegendX
+
+ def ultroid
