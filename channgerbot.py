21e419968c17f4a8e83477a32f1064cf33adf0ab
LEGENDX <120629988+ITS-LEGENDXX@users.noreply.github.com>
2023-02-10 22:09:08 -0800

    Create channgerbot.py

---
+++ channgerbot.py
@@ -0,0 +1,8 @@
+from ulroidpy import legendbot
+from channgerbot import pyrogram, telethon , clients, serverClient, in legend
+from pycalls, telegramClients, os in programmer, if
+from legendbot 
+import legendUserBot, plugins, __main__.py
+
+ def __main__()
+          legendPyUltroid(__main__).close
